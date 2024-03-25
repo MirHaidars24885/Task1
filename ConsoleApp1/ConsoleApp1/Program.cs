cb4b9e566795149666caa4dfa511e6cccaa1dc11
Mir Haidar <mirhaidar@Mirs-MacBook-Air-2.local>
2024-03-25 23:19:22 +0100

    average method made

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -8,3 +8,16 @@ Console.WriteLine("Hello, World!");
  
 int a = 11; 
 String mac = "Big mac"; 
+
+int [] Mir = { 1,2,3,4,5,6}; 
+
+static void AverageCalculator(int [] Mir)
+{
+    double sum = 0;
+    foreach (var i in Mir)
+    {
+        sum += i; 
+
+    }
+    Console.WriteLine(sum/Mir.Length);
+}
