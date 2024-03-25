81e0ea88cd0bdb37ced1fa7cbfceab5b57df9b3a
Mir Haidar <mirhaidar@Mirs-MacBook-Air-2.local>
2024-03-25 23:31:48 +0100

    method edit done

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -14,9 +14,9 @@ int [] Mir = { 1,2,3,4,5,6};
 static void AverageCalculator(int[] Mir)
 {
     double sum = 0;
-    foreach (var i in Mir)
+    foreach (var j in Mir)
     {
-        sum += i;
+        sum += j;
 
     }
 
