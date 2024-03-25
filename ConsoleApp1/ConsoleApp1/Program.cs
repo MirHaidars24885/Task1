6bd4974e853a00d832046252a6f33b4c0b22c37c
Mir Haidar <mirhaidar@Mirs-MacBook-Air-2.local>
2024-03-25 23:28:16 +0100

    maxvalue method made

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -9,15 +9,31 @@ Console.WriteLine("Hello, World!");
 int a = 11; 
 String mac = "Big mac"; 
 
-int [] Mir = { 1,2,3,4,5,6}; 
+int [] Mir = { 1,2,3,4,5,6};
 
-static void AverageCalculator(int [] Mir)
+static void AverageCalculator(int[] Mir)
 {
     double sum = 0;
     foreach (var i in Mir)
     {
-        sum += i; 
+        sum += i;
 
     }
-    Console.WriteLine(sum/Mir.Length);
+
+    Console.WriteLine(sum / Mir.Length);
+
+}
+
+static void returnsTheMaximumValue(int[] Mir)
+{
+    int maxval = Mir[0];
+    foreach (var j in Mir)
+    {
+        if (maxval < j)
+        {
+            maxval = j; 
+        }
+
+    }
+    Console.WriteLine(maxval);
 }
